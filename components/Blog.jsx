e0ad3e92e5e3b22eb8746090fedb6018442c7200
Saad <byiringirosaad@gmail.com>
2022-07-16 17:00:28 +0200

    Update Blog.jsx

--- components/Blog.jsx
+++ components/Blog.jsx
@@ -1,25 +1,30 @@
 import React from "react";
-import Image from "next/image";
 
 import { BiLinkExternal } from "react-icons/bi";
 
 import styles from "../styles/components/Blog.module.css";
 
-const Blog = ({ blog }) => {
+const Blog = ({ blog, width }) => {
   return (
     <div className={styles.container}>
-      <div className={styles.image}>
-        <Image src={blog?.image} alt="blog" width="100" height="100" />
-      </div>
-      <div className={styles.content}>
-        <p className={styles.link}>
-          <BiLinkExternal className={styles.icon} />
-          {blog?.link}
-        </p>
-        <p className={styles.title}>{blog?.title}</p>
-        <div className={styles.description}>{blog?.description}</div>
-      </div>
-      <div className={styles.category}>{blog?.category}</div>
+      {width > 600 ? (
+        <>
+          <div className={styles.image}>
+            <img src={blog?.image} alt="blog" loading="lazy" />
+          </div>
+          <div className={styles.content}>
+            <p className={styles.link}>
+              <BiLinkExternal className={styles.icon} />
+              {blog?.link}
+            </p>
+            <p className={styles.title}>{blog?.title}</p>
+            <div className={styles.description}>{blog?.description}</div>
+          </div>
+          <div className={styles.category}>{blog?.category}</div>
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
