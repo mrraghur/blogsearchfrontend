fa687fc779850e44d62ffb033c68f8a83a4b86f0
Byiringiro-saad <byiringirosaad@gmail.com>
2022-09-13 11:59:53 +0200

    delete old codebase

--- components/Ad.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import styles from "../styles/components/Ad.module.css";
-
-const Ad = (props) => {
-  return <div className={styles.container}>{props.children}</div>;
-};
-
-export default Ad;
--- components/Text.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Text = ({ doc }) => {
-  return React.createElement("div", {}, doc);
-};
-
-export default Text;
